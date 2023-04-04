30f5b5ca7734cbd2d725b0992ecf1369b00c298a
Richard5656 <80961838+Richard5656@users.noreply.github.com>
2023-04-03 21:19:14 -0700

    Update tforth.py

--- tforth.py
+++ tforth.py
@@ -40,7 +40,6 @@ BEGIN
 UNTIL
 )
 """
-
 stk = [0 for i in range(0,16000)]
 
 
@@ -52,29 +51,29 @@ def printn(s):
     print(s,end = "")
 
 pc = 0
-label_jmp_flag = 0 
+label_jmp_flag = 0
 varible_pointer_counter = 10000
 string_pointer_counter = 8000
 
 user_def_word = {}
 primative_words = {}
-primative_words["."] = lambda : printn(str(stk.pop())) 
+primative_words["."] = lambda : printn(str(stk.pop()))
 primative_words["EMIT"] = lambda : printn(chr(stk.pop()))
 primative_words["DUP"] = lambda : stk.append(stk[-1]);
 primative_words["DROP"] = lambda : stk.pop();
 
 
 
-def ADD(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 + op1) 
-def SUB(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 - op1) 
-def MUL(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 * op1) 
-def DIV(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 / op1) 
-def LESS(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 < op1) 
+def ADD(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 + op1)
+def SUB(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 - op1)
+def MUL(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 * op1)
+def DIV(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 / op1)
+def LESS(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 < op1)
 def GREATER(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 > op1)
 def EQUAL(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 == op1)
-def AND(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 & op1) 
-def OR(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 | op1) 
-def XOR(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 ^ op1) 
+def AND(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 & op1)
+def OR(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 | op1)
+def XOR(): global stk; op1 = stk.pop();op2 = stk.pop();stk.append( op2 ^ op1)
 def INVERT(): global stk; op1 = stk.pop();stk.append( not op1)
 def DEREF(): global stk; op1 = stk.pop();stk.append(stk[op1])
 def WRITEMEM(): global stk; op1 = stk.pop(); stk[op1]= stk.pop()
@@ -84,13 +83,13 @@ def IMPORT():
     tokens_external = open(tokens[pc],'r').read().replace("\\n","\n").upper().replace('\n'," ").replace('\t'," ").replace('\r'," ").strip().split(" ")
     for i in range(0,tokens_external.count("")):   #remove white space
         tokens_external.remove("")
-    
+   
     pc-=1
     tokens.pop(pc)
     tokens.pop(pc)
     for i in range(0,len(tokens_external)):
         tokens.insert(pc+i,tokens_external[i])
-    
+   
 
 def STRING():
     global string_pointer_counter, tokens,pc, stk
@@ -109,15 +108,15 @@ def STRING():
 def FLPJF(): # flips jump flag forcing program to jump wherever you want
     global label_jmp_flag
     label_jmp_flag ^= 1
-    
-    
-    
+   
+   
+   
 def LBL(): global label_jmp_flag; label_jmp_flag = 1
 
 primative_words["IMPORT"] = IMPORT    
 primative_words["+"] = ADD
 primative_words["-"] = SUB
-primative_words["*"] = MUL 
+primative_words["*"] = MUL
 primative_words["/"] = DIV
 primative_words["<"] = LESS
 primative_words[">"] = GREATER
@@ -145,53 +144,95 @@ primative_words["EBLK"] = lambda : 0
 def RKBLK(): # returns to SBLK
     global pc,tokens
     flag_ = 0
-    
+    pc-=1
     while(tokens[pc] != "SBLK" or flag_ != 0):
-            if(tokens[pc] in user_def_word and 
-            user_def_word[tokens[pc]]["type"] == "mf" and 
+            if(tokens[pc] in user_def_word and
+            user_def_word[tokens[pc]]["type"] == "mf" and
             (not (tokens[pc] in user_def_word[tokens[pc]]["exp"])) ): expand_mf() ; pc+=1
             pc-=1
-            if(tokens[pc] == "EBLK"): flag_ +=1
-            if(tokens[pc] == "SBLK" and flag_ != 0): flag_ -=1
             
+            if(tokens[pc] == "EBLK"): flag_ +=1            
+            if(tokens[pc] == "SBLK"): flag_ -=1
+
 def SKBLK(): # skips block by Jumping to EBLK
     global pc,tokens
     flag_ = 0
-    pc-=1
     while(tokens[pc] != "EBLK"or flag_ != 0):
-            if(tokens[pc] in user_def_word and 
-            user_def_word[tokens[pc]]["type"] == "mf" and 
+            if(tokens[pc] in user_def_word and
+            user_def_word[tokens[pc]]["type"] == "mf" and
             (not (tokens[pc] in user_def_word[tokens[pc]]["exp"])) ):expand_mf() ; pc-=1;
             pc+=1
-            if(tokens[pc] == "EBLK" and flag_ != 0): flag_ -=1
+            if(tokens[pc] == "EBLK"): flag_ -=1
             if(tokens[pc] == "SBLK"): flag_ +=1
 def RKBLKC(): # returns to SBLK if condition
     global pc,tokens,label_jmp_flag
     if(stk.pop()):
         RKBLK()
-        
-        
+       
+       
 def SKBLKC(): # skips block by Jumping to EBLK
     global pc,tokens,label_jmp_flag
     if(stk.pop()):
         SKBLK()
 
+
+
+
+
+def SRKBLK(): # returns to SBLK
+    global pc,tokens
+    flag_ = 0
+    while(tokens[pc] != "SBLK" or flag_ != 0):
+            if(tokens[pc] in user_def_word and
+            user_def_word[tokens[pc]]["type"] == "mf" and
+            (not (tokens[pc] in user_def_word[tokens[pc]]["exp"])) ): expand_mf() ; pc+=1
+            pc-=1
+            if(tokens[pc] == "EBLK" ): flag_ +=1
+            if(tokens[pc] == "SBLK"and flag_ != 0): flag_ -=1
+           
+def SSKBLK(): # skips block by Jumping to EBLK
+    global pc,tokens
+    flag_ = 0
+    while(tokens[pc] != "EBLK"or flag_ != 0):
+            if(tokens[pc] in user_def_word and
+            user_def_word[tokens[pc]]["type"] == "mf" and
+            (not (tokens[pc] in user_def_word[tokens[pc]]["exp"])) ):expand_mf() ; pc-=1;
+            pc+=1
+            if(tokens[pc] == "EBLK"and flag_ != 0): flag_ -=1
+            if(tokens[pc] == "SBLK" ): flag_ +=1
+def SRKBLKC(): # returns to SBLK if condition
+    global pc,tokens,label_jmp_flag
+    if(stk.pop()):
+        SRKBLK()
+       
+       
+def SSKBLKC(): # skips block by Jumping to EBLK
+    global pc,tokens,label_jmp_flag
+    if(stk.pop()):
+        SSKBLK()
+
+
+
+
 primative_words["RKBLK"] = RKBLK
 primative_words["SKBLK"] = SKBLK
 primative_words["RKBLKC"] = RKBLKC
 primative_words["SKBLKC"] = SKBLKC
-
+primative_words["SRKBLK"] = SRKBLK
+primative_words["SSKBLK"] = SSKBLK
+primative_words["SRKBLKC"] = SRKBLKC
+primative_words["SSKBLKC"] = SSKBLKC
 
 def B():
     global pc,tokens,label_jmp_flag
-        
-    
+       
+   
     if(label_jmp_flag == 1):
         pc-=1
         while(tokens[pc] != "LBL"):
-            if(tokens[pc] in user_def_word and 
-            user_def_word[tokens[pc]]["type"] == "mf" and 
-            (not (tokens[pc] in user_def_word[tokens[pc]]["exp"])) ): 
+            if(tokens[pc] in user_def_word and
+            user_def_word[tokens[pc]]["type"] == "mf" and
+            (not (tokens[pc] in user_def_word[tokens[pc]]["exp"])) ):
                 expand_mf()
                 pc+=1
             pc-=1
@@ -199,9 +240,9 @@ def B():
     else:
         pc+=1
         while(tokens[pc] != "LBL"):
-            if(tokens[pc] in user_def_word and 
-            user_def_word[tokens[pc]]["type"] == "mf" and 
-            (not (tokens[pc] in user_def_word[tokens[pc]]["exp"])) ): 
+            if(tokens[pc] in user_def_word and
+            user_def_word[tokens[pc]]["type"] == "mf" and
+            (not (tokens[pc] in user_def_word[tokens[pc]]["exp"])) ):
                 expand_mf()
                 pc-=1
             pc+=1
@@ -232,7 +273,7 @@ def expand_mf():
 
 #eval mode
 def eval_forth():
-  
+ 
     global pc,stk,primative_words,user_def_word,varible_pointer_counter
     while pc < len(tokens):
         curtok = get_tok()
@@ -270,6 +311,7 @@ def eval_forth():
             print(f"User word has not been defined yet: {curtok}")
             exit(-1)
 eval_forth()
+print()
 #print(user_def_word)
 #print(stk)
 #print(tokens)
